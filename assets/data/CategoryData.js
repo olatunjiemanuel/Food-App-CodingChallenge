a9d4738ede49f57d62cfa6cfeb14503507895e7f
firstChairCoder <md1qq7@gmail.com>
2021-12-17 14:16:46 +0100

    slight changes

---
+++ assets/data/CategoryData.js
@@ -0,0 +1,17 @@
+const CategoryData = [
+    {
+      id: 'Category-1',
+      text: 'All',
+      isActive: true
+    },
+    {
+      id: 'Category-2',
+      text: 'Food',
+    },
+    {
+      id: 'Category-3',
+      text: 'Drink',
+    }
+  ];
+  
+  export default CategoryData;
